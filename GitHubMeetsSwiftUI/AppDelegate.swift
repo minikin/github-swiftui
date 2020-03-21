4d05ea1d341a86e1474c691ea89ebe6d9eff6625
minikin <djminikin@gmail.com>
2020-03-21 12:37:28 +0100

    Inital commit

---
+++ GitHubMeetsSwiftUI/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  GitHubMeetsSwiftUI
+//
+//  Created by Sasha Prokhorenko on 21.03.20.
+//  Copyright © 2020 Sasha Prokhorenko. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+      
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+
+    }
+
+
+}
+
---
+++ GitHubMeetsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubMeetsSwiftUI
+//
+//  Created by Sasha Prokhorenko on 21.03.20.
+//  Copyright © 2020 Sasha Prokhorenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
